92118baba2bf6b88aa8be138fa913f07b7d0987f
Tom Dilatush <tom@dilatush.com>
2018-08-21 13:31:30 -0600

    Finished deployment support, fixed a minor logging bug...

--- deployCPO.sh
+++ deployCPO.sh
@@ -1,10 +1,18 @@
 #! /bin/bash
+
+# copy all the deployment files over to the beast...
 scp out/artifacts/CPO.jar tom@beast:/apps/cpo
 scp CPO.service tom@beast:/apps/cpo
 scp CPOConfigTom.json tom@beast:/apps/cpo
 scp CPOLog.json tom@beast:/apps/cpo
 scp runCPO.sh tom@beast:/apps/cpo
-ssh tom@beast << ENDSSH
+
+# execute commands on the beast
+# get to the app directory
+# set mode and owner on files that stay in that directory
+# copy the service wrapper (if it has changed) to the systemd/system directory, change its mode and owner
+# bounce the CPO service
+ssh tom@beast << RUN_ON_BEAST
 cd /apps/cpo
 sudo chown cpo:cpo CPO.jar
 sudo chmod ug+xrw CPO.jar
@@ -17,4 +25,7 @@ sudo chmod ug+xrw runCPO.sh
 sudo cp -u CPO.service /etc/systemd/system
 sudo chown cpo:cpo /etc/systemd/system/CPO.service
 sudo chmod ug+xrw /etc/systemd/system/CPO.service
-ENDSSH
+sudo systemctl stop CPO.service
+sudo systemctl daemon-reload
+sudo systemctl start CPO.service
+RUN_ON_BEAST
